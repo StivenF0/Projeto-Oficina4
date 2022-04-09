cb813d052b7fff8ea4483f4653b6594b7b7a938e
StivenF0 <stivenfonseca@gmail.com>
2022-04-09 16:37:29 -0300

    making the app

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+progressBar = document.getElementById('progressBar')
+value = document.getElementById('progressValue')
+
+const progressSpeed = 15; //Speed in ms
+let progressValue = 0;
+let progressValueEnd = 30; // Value in percent
+
+progress = setInterval(() => {
+    value.textContent = `${progressValue}`
+    progressBar.style.background = `conic-gradient(
+        var(--primary-color) ${progressValue * 3.6}deg,
+        var(--secondary-color) ${progressValue * 3.6}deg
+    )`
+    if (progressValue === progressValueEnd) {
+        clearInterval(progress)
+    } else {
+        progressValue++
+    }
+}, progressSpeed)
+
+obj = document.querySelector('main > div:nth-child(2)')
+console.log(obj)
