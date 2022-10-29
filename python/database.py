81f32084ec2ee613379a58c10bf7d721c8b1fc10
Eduardo Aquino da Silva <eduaquino14@hotmail.com>
2022-10-29 17:38:44 -0300

    null for default in python

--- python/database.py
+++ python/database.py
@@ -10,6 +10,6 @@ def register(data):
     """
     now = datetime.now()
     str_date = now.strftime('%Y-%m-%d %H:%M:%S')
-    connection.cursor.execute('INSERT INTO registers VALUES (null, %s, %s, null)', (str_date, data))
+    connection.cursor.execute('INSERT INTO registers VALUES (default, %s, %s, null)', (str_date, data))
     connection.my_database.commit()
     print('Register done succesfully')
